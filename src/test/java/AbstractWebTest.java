98aae41ae8f1e0082ff47c2254fe376e8e164db2
zyake <zyake.mk4@gmail.com>
2014-01-19 22:34:30 +0900

    コミット

---
+++ src/test/java/AbstractWebTest.java
@@ -0,0 +1,78 @@
+import com.google.common.base.Predicate;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * PhantomJSを使うWebテストケースの基底クラス。
+ *
+ * <p>
+ * システムプロパティから以下の値を読み込み、テストケース内で使用する。
+ * </p>
+ * <ul>
+ *     <li>my.apps.test.timeout: DOM要素の出現を待ち受ける時間(ミリ秒)</li>
+ *     <li>my.apps.test.appurl: テスト対象アプリのURL</li>
+ *     <li>my.apps.test.phantomjspath: PhantomJSのパス</li>
+ * </ul>
+ */
+public abstract class AbstractWebTest {
+
+    private long timeout;
+
+    private String appUrl;
+
+    private String phantomJsPath;
+
+    private WebDriver driver;
+
+    public AbstractWebTest() {
+        Properties sysProps = System.getProperties();
+
+        String timeout = sysProps.getProperty("my.apps.test.timeout", "15");
+        this.timeout = Long.parseLong(timeout);
+
+        appUrl = sysProps.getProperty("my.apps.test.appurl");
+
+        phantomJsPath = sysProps.getProperty("my.apps.test.phantomjspath", "resource/phantomjs_windows");
+    }
+
+    @Before
+    public void before() {
+        driver = createWebDriver();
+    }
+
+    public long getTimeout() {
+        return timeout;
+    }
+
+    public String getAppUrl() {
+        return appUrl;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void waitUntil(Predicate<WebDriver> predicate, String msg) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeout);
+        webDriverWait.withMessage(msg);
+        webDriverWait.until(predicate);
+    }
+
+    private WebDriver createWebDriver() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                phantomJsPath);
+        caps.setJavascriptEnabled(true);
+        WebDriver driver = new PhantomJSDriver(caps);
+        driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+
+        return driver;
+    }
+}
---
+++ src/test/java/SolrUITest.java
@@ -0,0 +1,105 @@
+import com.google.common.base.Predicate;
+import com.sun.istack.internal.Nullable;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SolrUITest extends AbstractWebTest {
+
+    @Test
+    public void testInitialLoad() throws InterruptedException {
+        getDriver().get(getAppUrl());
+
+        waitUntil(new Predicate<WebDriver>() {
+            @Override
+            public boolean apply(@Nullable WebDriver webDriver) {
+                WebElement fieldFacets = webDriver.findElement(By.className("field_facets"));
+
+                List<WebElement> h3Elems = fieldFacets.findElements(By.tagName("h3"));
+                boolean allCategoriesAreLoaded = h3Elems.size()  == 2;
+
+                List<WebElement> liElems = fieldFacets.findElements(By.tagName("li"));
+                boolean allIndexesAreLoaded = liElems.size() == 5;
+
+                return allCategoriesAreLoaded && allIndexesAreLoaded;
+            }
+        }, "The initial contents should exist.");
+    }
+
+    @Test
+    public void testSearchDocThatExistsMoreThanInitialItemCount() throws InterruptedException {
+        getDriver().get(getAppUrl());
+
+        WebElement searchBox = getDriver().findElement(By.cssSelector("#searchBox .input"));
+        searchBox.sendKeys("BLogic");
+
+        WebElement submit = getDriver().findElement(By.cssSelector("#searchBox .submit"));
+        submit.click();
+
+        waitUntil(new Predicate<WebDriver>() {
+            @Override
+            public boolean apply(@Nullable WebDriver webDriver) {
+                WebElement resultSummary = webDriver.findElement(By.cssSelector("#searchBox .resultSummary"));
+                boolean isExpectedSummary = resultSummary.getText().startsWith(
+                        "検索が完了しました.(マッチしたもの=7, 取得したもの=5, 検索時間=");
+
+                List<WebElement> docElems = webDriver.findElements(By.cssSelector(".content .document"));
+                boolean isExpectedDocElemCount = docElems.size() == 5;
+
+                return isExpectedSummary & isExpectedDocElemCount;
+            }
+        }, "The search result should be matched.");
+    }
+
+    @Test
+    public void downloadDocument() throws IOException, NoSuchAlgorithmException {
+        getDriver().get(getAppUrl());
+
+        WebElement searchBox = getDriver().findElement(By.cssSelector("#searchBox .input"));
+
+        // determine only one document.
+        searchBox.sendKeys("id:*アーキテクチャ説明書.pdf AND id:*Rich版*");
+
+        WebElement submit = getDriver().findElement(By.cssSelector("#searchBox .submit"));
+        submit.click();
+
+        ExpectedConditions.textToBePresentInElementValue(
+                By.cssSelector("#searchBox .resultSummary"),
+                "検索が完了しました.");
+
+        WebElement docElem = getDriver().findElement(By.cssSelector(".content .document"));
+        WebElement anchorElem = docElem.findElement(By.tagName("a"));
+        String href = anchorElem.getAttribute("href");
+
+        // compare file digests.
+        URL url = new URL(href);
+        byte[]  loadedFileDigest = getDigest(url.openStream());
+        byte[]  fileDigest = getDigest(
+                   new FileInputStream("resource/expectedFile.pdf"));
+
+        assertArrayEquals(fileDigest, loadedFileDigest);
+    }
+
+    private byte[] getDigest(InputStream inputStream) throws IOException, NoSuchAlgorithmException {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        try (DigestInputStream digestInputStream = new DigestInputStream(inputStream, md5) ) {
+            while ( digestInputStream.read() != -1 ) {
+            }
+
+            return md5.digest();
+        }
+    }
+}
